161b0f393c61722423375777bff647939231d12d
ThePrimeagen <mpaulson@netflix.com>
2021-07-09 11:15:27 -0600

    automagic messaging from me in the past.  Have you checked up your butthole?

--- nvim/.config/nvim/lua/theprimeagen/telescope.lua
+++ nvim/.config/nvim/lua/theprimeagen/telescope.lua
@@ -31,7 +31,8 @@ local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
         prompt_title = "< VimRC >",
-        cwd = "$HOME/dotfiles/awesome-streamerrc/ThePrimeagen/",
+        cwd = vim.env.DOTFILES,
+        hidden = true,
     })
 end
 
