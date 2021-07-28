2075f2defa483559b93725a22d047160203e69ab
ThePrimeagen <mpaulson@netflix.com>
2021-07-28 07:55:17 -0600

    automagic messaging from me in the past.  Have you checked up your butthole?

--- nvim/.config/nvim/lua/theprimeagen/telescope.lua
+++ nvim/.config/nvim/lua/theprimeagen/telescope.lua
@@ -1,31 +1,31 @@
-local actions = require('telescope.actions')
-require('telescope').setup {
+local actions = require("telescope.actions")
+require("telescope").setup({
     defaults = {
-        file_sorter = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix = ' >',
+        file_sorter = require("telescope.sorters").get_fzy_sorter,
+        prompt_prefix = " >",
         color_devicons = true,
 
-        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
-        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
-        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
         mappings = {
             i = {
                 ["<C-x>"] = false,
                 ["<C-q>"] = actions.send_to_qflist,
             },
-        }
+        },
     },
     extensions = {
         fzy_native = {
             override_generic_sorter = false,
             override_file_sorter = true,
-        }
-    }
-}
+        },
+    },
+})
 
 require("telescope").load_extension("git_worktree")
-require('telescope').load_extension('fzy_native')
+require("telescope").load_extension("fzy_native")
 
 local M = {}
 M.search_dotfiles = function()
@@ -36,42 +36,30 @@ M.search_dotfiles = function()
     })
 end
 
-            -- map(mode, key, lua function to call)
-            --
-            -- good place to look: telescope.actions (init.lua)
-            -- lua function to call:  (gets bufnr, not necessarily needed)
-            --   require('telescope.actions.state').get_selected_entry(bufnr)
-            --   Actions just take the bufnr and give out information
-            --   require('telescope.actions').close(bufnr)
-            --
-            --   check out telescope.actions for _all the available_ supported
-            --   actions.
-            --
-            --   :h telescope.setup ->
-            --   :h telescope.builtin ->
-            --   :h telescope.layout ->
-            --   :h telescope.actions
-            --
-function set_background(content)
+local function set_background(content)
     vim.fn.system(
-        "dconf write /org/mate/desktop/background/picture-filename \"'" .. content .. "'\"")
+        "dconf write /org/mate/desktop/background/picture-filename \"'"
+            .. content
+            .. "'\""
+    )
 end
 
 local function select_background(prompt_bufnr, map)
     local function set_the_background(close)
-        local content =
-        require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+        local content = require("telescope.actions.state").get_selected_entry(
+            prompt_bufnr
+        )
         set_background(content.cwd .. "/" .. content.value)
         if close then
-            require('telescope.actions').close(prompt_bufnr)
+            require("telescope.actions").close(prompt_bufnr)
         end
     end
 
-    map('i', '<C-p>', function()
+    map("i", "<C-p>", function()
         set_the_background()
     end)
 
-    map('i', '<CR>', function()
+    map("i", "<CR>", function()
         set_the_background(true)
     end)
 end
@@ -88,7 +76,7 @@ local function image_selector(prompt, cwd)
                 -- Please continue mapping (attaching additional key maps):
                 -- Ctrl+n/p to move up and down the list.
                 return true
-            end
+            end,
         })
     end
 end
@@ -96,15 +84,37 @@ end
 M.anime_selector = image_selector("< Anime Bobs > ", "~/dotfiles/backgrounds")
 M.chat_selector = image_selector("< Chat Sucks > ", "~/dotfiles/chat")
 
+local function refactor(prompt_bufnr)
+    local content = require("telescope.actions.state").get_selected_entry(
+        prompt_bufnr
+    )
+    require("telescope.actions").close(prompt_bufnr)
+    require("refactoring").refactor(content.value)
+end
+
+M.refactors = function()
+    require("telescope.pickers").new({}, {
+        prompt_title = "refactors",
+        finder = require("telescope.finders").new_table({
+            results = require("refactoring").get_refactors(),
+        }),
+        sorter = require("telescope.config").values.generic_sorter({}),
+        attach_mappings = function(_, map)
+            map("i", "<CR>", refactor)
+            map("n", "<CR>", refactor)
+            return true
+        end
+    }):find()
+end
+
 M.git_branches = function()
     require("telescope.builtin").git_branches({
         attach_mappings = function(_, map)
-            map('i', '<c-d>', actions.git_delete_branch)
-            map('n', '<c-d>', actions.git_delete_branch)
+            map("i", "<c-d>", actions.git_delete_branch)
+            map("n", "<c-d>", actions.git_delete_branch)
             return true
-        end
+        end,
     })
 end
 
 return M
-
--- nvim/.config/nvim/plugin/telescope.vim
+++ nvim/.config/nvim/plugin/telescope.vim
@@ -1,5 +1,7 @@
 lua require("theprimeagen")
 
+nnoremap <leader>rr :lua require('theprimeagen.telescope').refactors()<CR>
+vnoremap <leader>rr :lua require('theprimeagen.telescope').refactors()<CR>
 nnoremap <leader>ps :lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>
 nnoremap <C-p> :lua require('telescope.builtin').git_files()<CR>
 nnoremap <Leader>pf :lua require('telescope.builtin').find_files()<CR>
