4c21c86bf7547040ed4b277aa324ad15a257c727
Balázs Szabó <hobord@users.noreply.github.com>
2021-09-14 17:42:05 +0300

    use pager against loop

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -4,10 +4,8 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    echo "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
-    tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
 else
-    echo "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
-    tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
 
