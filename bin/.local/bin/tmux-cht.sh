2d564e836c062b8a2e77e54a3adfd8276333e9ef
ThePrimeagen <mpaulson@netflix.com>
2021-09-15 18:56:35 -0600

    feat

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
