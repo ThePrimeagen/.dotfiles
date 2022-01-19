ab48b65b5cee684fff1bdbbbc7e6f6210d1df3a1
Chris Clarkson <chrisclarkson@improbable.io>
2022-01-19 08:32:40 +0000

    fix: updated cheatsheet command to use updated url

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+selected=$(cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
     exit 0
 fi
@@ -7,9 +7,8 @@ fi
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    query=$(echo $query | tr ' ' '+')
+    tmux neww bash -c "echo \"curl cheat.sh/$selected/$query/\" & curl cheat.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cheat.sh/$selected~$query | less"
 fi
-
