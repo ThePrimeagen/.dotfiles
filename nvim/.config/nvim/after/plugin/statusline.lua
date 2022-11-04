bdd93f0de4da4885bfe3a8204dbb51a2a1f512b1
mpaulson <mpaulson@netflix.com>
2022-11-04 07:58:38 -0600

    feat: update to the packer / statusline

--- nvim/.config/nvim/after/plugin/statusline.lua
+++ nvim/.config/nvim/after/plugin/statusline.lua
@@ -13,6 +13,9 @@ local sections = { lualine_a = { hello } }
 
 require('lualine').setup({
     sections = sections,
+    options = {
+        icons_enabled = false,
+    }
 })
 
 
--- nvim/.config/nvim/lua/theprimeagen/packer.lua
+++ nvim/.config/nvim/lua/theprimeagen/packer.lua
@@ -11,7 +11,6 @@ return require("packer").startup(function(use)
 
     use({
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     })
 
     -- All the things
